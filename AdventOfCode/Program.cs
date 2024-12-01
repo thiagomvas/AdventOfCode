6337319cb7fe337953854ba1718afc900905f695
Thiago Menezes <thiagomvas@gmail.com>
2024-12-01 18:17:21 -0300

    Add project files.

---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,45 @@
+﻿using AdventOfCode;
+
+Console.WriteLine("Enter formatted yyyy/dd solution number (e.g. 2024/01):");
+
+var input = Console.ReadLine();
+if (input is null)
+{
+    Console.WriteLine("Invalid input.");
+    return;
+}
+
+var parts = input.Split('/');
+
+if (parts.Length != 2)
+{
+    Console.WriteLine("Invalid input.");
+    return;
+}
+
+var year = parts[0];
+var day = parts[1].TrimStart('0');
+
+// Use reflection to get the solution
+var solutionType = Type.GetType($"AdventOfCode.Solutions._{year}.Day{day}");
+
+if (solutionType is null)
+{
+    Console.WriteLine("Solution not found.");
+    return;
+}
+
+var solution = Activator.CreateInstance(solutionType) as IDaySolution;
+
+if (solution is null)
+{
+    Console.WriteLine("Solution not found.");
+    return;
+}
+
+// Read the input file if it exists
+var inputfile = File.ReadAllText($"Inputs/{year}/Day{day}.txt");
+
+var result = solution.Solve(inputfile);
+
+Console.WriteLine(result);
---
+++ AdventOfCode/Solutions/2024/Day1.cs
@@ -0,0 +1,8 @@
+﻿namespace AdventOfCode.Solutions._2024;
+internal class Day1 : IDaySolution
+{
+    public string Solve(string input)
+    {
+        return input;
+    }
+}
